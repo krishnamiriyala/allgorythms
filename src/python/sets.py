de64034b466805adc2cd22f37e437def59a8cf90
Krishna Miriyala <krishnambm@gmail.com>
2020-01-07 05:06:17 +0000

    Add sets in python

---
+++ src/python/sets.py
@@ -0,0 +1,14 @@
+def sets():
+    print({1, 2, 3, 4})         # Set
+    print(set([1, 2, 3, 4]))    # Set from a list
+    print(set((1, 2, 3, 4)))    # Set from a tuple
+
+    print(type({}))             # {} is empty dict and not set
+    print(type(set()))          # For empty set use set()
+
+    inp = [1, 2, 3, 4, 4, 1, 1, 2, 2]
+    print(set(inp))             # set removes dups if input has them
+
+
+if __name__ == '__main__':
+    sets()
