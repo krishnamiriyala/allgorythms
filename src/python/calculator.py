f4a65979d2a305b182517768c012a6665d6e98d5
Krishna Miriyala <krishnambm@gmail.com>
2020-01-09 04:23:05 +0000

    Add countdown in python

--- src/python/calculator.py
+++ src/python/calculator.py
@@ -1,13 +1,13 @@
 def add(x, y):
-    return x+y
+    return x + y
 
 
 def subtract(x, y):
-    return x-y
+    return x - y
 
 
 def multiply(x, y):
-    return x*y
+    return x * y
 
 
 def calculator():
---
+++ src/python/countdown.py
@@ -0,0 +1,7 @@
+def countdown(count: int):
+    for num in range(count, 0, -1):
+        print(num)
+
+
+if __name__ == '__main__':
+    countdown(10)
